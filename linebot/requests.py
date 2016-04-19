c7d9d390846f1119db44d56d0601a835c639c1d4
studio3104 <studio3104.com@gmail.com>
2016-04-19 15:13:55 +0900

    adds requests.py as just a skelton

---
+++ linebot/requests.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+class Request():
+    def __init__(self, **kwargs):
+        pass
+
+    def validate(self):
+        pass
+
+    def post(self):
+        pass
---
+++ tests/test_requests.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from linebot.requests import Request
+
+
+class TestRequest():
+    def test_request_instance_creation(self):
+        Request()

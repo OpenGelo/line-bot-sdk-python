cc3934e1b46453b6bd683883ab791478dfc32d88
studio3104 <studio3104.com@gmail.com>
2016-04-19 17:41:28 +0900

    adds concrete implementations to linebot/messages.py

--- linebot/messages.py
+++ linebot/messages.py
@@ -2,9 +2,25 @@
 
 
 class MessageBase(object):
-    pass
+    def __init__(self, **attrs):
+        self._attrs = attrs
+        self.content = self._create_content()
+        self.event_type = '138311608800106203'
+
+    def _create_content(self):
+        raise NotImplementedError
+
+    def is_valid(self):
+        raise NotImplementedError
 
 
 class TextMessage(MessageBase):
-    def __init__(self, text):
-        pass
+    def _create_content(self):
+        return {
+            'contentType': 1,  # Fixed value
+            'toType': 1,  # 1 => user
+            'text': self._attrs['text'],
+        }
+
+    def is_valid(self):
+        return self._attrs['text'] is not None
--- tests/test_messages.py
+++ tests/test_messages.py
@@ -5,4 +5,11 @@ from linebot import messages
 
 class TestMessages():
     def test_text_message(self):
-        messages.TextMessage(text='')
+        test_text = 'TEST TEXT MESSAGE'
+        message = messages.TextMessage(text=test_text)
+        assert message.event_type == '138311608800106203'
+        assert message.content == {
+            'contentType': 1,
+            'toType': 1,
+            'text': test_text,
+        }

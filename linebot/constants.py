d06679afb0eeb6710e53a1992a85ff1bcb149bc0
studio3104 <studio3104.com@gmail.com>
2016-04-20 16:59:41 +0900

    adds constants.ContentType

--- linebot/constants.py
+++ linebot/constants.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import enum
 from urlparse import urlparse, urljoin
 
 API_URL_BASE = 'https://trialbot-api.line.me'
@@ -8,3 +9,14 @@ API_VERSION = 'v1'
 API_URL_EVENTS = urljoin(__parsed_base_url.geturl(), '{}/{}'.format(API_VERSION, 'events'))
 
 TO_CHANNEL = 1383378250
+
+
+class ContentType(enum.IntEnum):
+    TEXT = 1
+    IMAGE = 2
+    VIDEO = 3
+    AUDIO = 4
+    LOCATION = 7
+    STICKER = 8
+    CONTACT = 10
+    RICH_MESSAGE = 12
--- tests/test_constants.py
+++ tests/test_constants.py
@@ -10,3 +10,13 @@ class TestConstants():
         assert constants.API_URL_EVENTS == 'https://trialbot-api.line.me/v1/events'
 
         assert constants.TO_CHANNEL == 1383378250
+
+    def test_content_type(self):
+        assert constants.ContentType.TEXT == 1
+        assert constants.ContentType.IMAGE == 2
+        assert constants.ContentType.VIDEO == 3
+        assert constants.ContentType.AUDIO == 4
+        assert constants.ContentType.LOCATION == 7
+        assert constants.ContentType.STICKER == 8
+        assert constants.ContentType.CONTACT == 10
+        assert constants.ContentType.RICH_MESSAGE == 12

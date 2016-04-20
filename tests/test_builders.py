d657ff3ee7b35791f6bbc6575cb1aeede6c98f20
studio3104 <studio3104.com@gmail.com>
2016-04-20 19:59:38 +0900

    adds content assertion

--- tests/test_builders.py
+++ tests/test_builders.py
@@ -19,49 +19,120 @@ class TestMultipleMessage():
         assert response.status_code == 200
 
     def test_add_text(self, fx_multiple_message):
-        multiple_message = fx_multiple_message.add_text(text='')
+        text = 'TEST TEXT MESSAGE'
+        multiple_message = fx_multiple_message.add_text(text=text)
         assert isinstance(multiple_message, MultipleMessage)
         assert id(fx_multiple_message) == id(multiple_message)
         assert multiple_message.is_valid()
         assert multiple_message.event_type == '140177271400161403'
+        assert multiple_message.content == {
+            'messageNotified': 0,
+            'messages': [
+                {
+                    'contentType': 1,
+                    'text': text,
+                    'toType': 1,
+                },
+            ],
+        }
 
     def test_add_image(self, fx_multiple_message):
+        image_url = 'image_url'
+        preview_url = 'preview_url'
         multiple_message = fx_multiple_message.add_image(
-            image_url='image_url',
-            preview_url='preview_url',
+            image_url=image_url,
+            preview_url=preview_url,
         )
         assert isinstance(multiple_message, MultipleMessage)
         assert id(fx_multiple_message) == id(multiple_message)
         assert multiple_message.is_valid()
         assert multiple_message.event_type == '140177271400161403'
+        assert multiple_message.content == {
+            'messageNotified': 0,
+            'messages': [
+                {
+                    'contentType': 2,
+                    'originalContentUrl': image_url,
+                    'previewImageUrl': preview_url,
+                    'toType': 1,
+                },
+            ],
+        }
 
     def test_add_video(self, fx_multiple_message):
+        video_url = 'video_url'
+        preview_url = 'preview_url'
         multiple_message = fx_multiple_message.add_video(
-            video_url='video_url',
-            preview_url='preview_url',
+            video_url=video_url,
+            preview_url=preview_url,
         )
         assert isinstance(multiple_message, MultipleMessage)
         assert id(fx_multiple_message) == id(multiple_message)
         assert multiple_message.is_valid()
         assert multiple_message.event_type == '140177271400161403'
+        assert multiple_message.content == {
+            'messageNotified': 0,
+            'messages': [
+                {
+                    'contentType': 3,
+                    'originalContentUrl': video_url,
+                    'previewImageUrl': preview_url,
+                    'toType': 1,
+                },
+            ],
+        }
 
     def test_add_audio(self, fx_multiple_message):
+        audio_url = 'audio_url'
+        duration = 240000
         multiple_message = fx_multiple_message.add_audio(
-            audio_url='audio_url',
-            duration='duration',
+            audio_url=audio_url,
+            duration=duration,
         )
         assert isinstance(multiple_message, MultipleMessage)
         assert id(fx_multiple_message) == id(multiple_message)
         assert multiple_message.is_valid()
         assert multiple_message.event_type == '140177271400161403'
+        assert multiple_message.content == {
+            'messageNotified': 0,
+            'messages': [
+                {
+                    'contentType': 4,
+                    'originalContentUrl': audio_url,
+                    'contentMetadata': {
+                        'AUDLEN': str(duration)
+                    },
+                    'toType': 1,
+                },
+            ],
+        }
 
     def test_add_location(self, fx_multiple_message):
+        title = 'Convention center'
+        latitude = 35.61823286112982
+        longitude = 139.72824096679688
         multiple_message = fx_multiple_message.add_location(
-            title='Convention center',
-            latitude=35.61823286112982,
-            longitude=139.72824096679688,
+            title=title,
+            latitude=latitude,
+            longitude=longitude,
         )
         assert isinstance(multiple_message, MultipleMessage)
         assert id(fx_multiple_message) == id(multiple_message)
         assert multiple_message.is_valid()
         assert multiple_message.event_type == '140177271400161403'
+        assert multiple_message.content == {
+            'messageNotified': 0,
+            'messages': [
+                {
+                    'contentType': 7,
+                    'text': title,
+                    'location': {
+                        'title': title,
+                        'address': None,
+                        'latitude': latitude,
+                        'longitude': longitude,
+                    },
+                    'toType': 1,
+                },
+            ],
+        }

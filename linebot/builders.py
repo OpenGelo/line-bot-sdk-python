b8b97efffa50dc9e561699a23e94e4d8d0f8e851
studio3104 <studio3104.com@gmail.com>
2016-04-20 17:06:36 +0900

    adds event_type as property

--- linebot/builders.py
+++ linebot/builders.py
@@ -5,3 +5,7 @@ class MultipleMessage():
     def __init__(self, client):
         self.messages = []
         self.client = client
+
+    @property
+    def event_type(self):
+        return '140177271400161403'

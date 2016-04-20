7811f37cadc1368852789e1af4c1a11e486bfe4b
studio3104 <studio3104.com@gmail.com>
2016-04-20 17:10:01 +0900

    adds content as property

--- linebot/builders.py
+++ linebot/builders.py
@@ -7,5 +7,12 @@ class MultipleMessage():
         self.client = client
 
     @property
+    def content(self):
+        return {
+            'messageNotified': 0,
+            'messages': self.messages,
+        }
+
+    @property
     def event_type(self):
         return '140177271400161403'

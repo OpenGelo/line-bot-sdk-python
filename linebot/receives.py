97649ef9ac067098294710109cc982416220d02c
studio3104 <studio3104.com@gmail.com>
2016-04-21 11:57:59 +0900

    adds concrete implementation on receives.Message

--- linebot/receives.py
+++ linebot/receives.py
@@ -2,7 +2,8 @@
 
 import json
 
-from linebot.constants import ReceiveEventType
+from linebot import messages
+from linebot.constants import ContentType, ReceiveEventType
 
 
 class Receive():
@@ -26,7 +27,56 @@ class Receive():
 
 class Message():
     def __init__(self, datum):
-        pass
+        self.__data = {
+            'id': datum['content']['id'],
+            'from_mid': datum['content']['from'],
+            'to_mid': datum['content']['to'],
+            'from_channel_id': datum['fromChannel'],
+            'to_channel_id': datum['toChannel'],
+            'event_type': datum['eventType'],
+            'created_time': datum['content']['createdTime'],
+            'content': self.__generate_content(datum['content']).content,
+        }
+
+    def __getitem__(self, key):
+        return self.__data[key]
+
+    def __generate_content(self, attrs):
+        content_type = attrs['contentType']
+        if content_type == ContentType.TEXT:
+            return messages.TextMessage(
+                text=attrs['text'],
+            )
+        elif content_type == ContentType.IMAGE:
+            return messages.ImageMessage(
+                image_url=attrs['originalContentUrl'],
+                preview_url=attrs['previewImageUrl'],
+            )
+        elif content_type == ContentType.VIDEO:
+            return messages.VideoMessage(
+                video_url=attrs['originalContentUrl'],
+                preview_url=attrs['previewImageUrl'],
+            )
+        elif content_type == ContentType.AUDIO:
+            return messages.AudioMessage(
+                audio_url=attrs['originalContentUrl'],
+                duration=attrs['contentMetadata']['duration'],
+            )
+        elif content_type == ContentType.Location:
+            return messages.LocationMessage(
+                title=attrs['location']['title'],
+                address=attrs['location']['address'],
+                latitude=attrs['location']['latitude'],
+                longitude=attrs['location']['longitude'],
+            )
+        elif content_type == ContentType.STICKER:
+            return messages.StickerMessage(
+                stkpkgid=attrs['contentMetadata']['STKPKGID'],
+                stkid=attrs['contentMetadata']['STKID'],
+                stkver=attrs['contentMetadata']['STKVER'],
+            )
+        else:
+            raise ValueError('Invalid content type.')
 
 
 class Operation():
--- tests/test_receives.py
+++ tests/test_receives.py
@@ -4,6 +4,18 @@ from linebot.receives import Receive
 
 
 class TestReceive():
-    def test_instance_creation(self, fx_json_request_content_text):
+    def test_instance_creation_text_message(self, fx_json_request_content_text):
         receive = Receive(fx_json_request_content_text)
         assert isinstance(receive, Receive)
+        assert receive[0]['id']
+        assert receive[0]['from_mid']
+        assert receive[0]['to_mid']
+        assert receive[0]['from_channel_id']
+        assert receive[0]['to_channel_id']
+        assert receive[0]['event_type']
+        assert receive[0]['created_time']
+        assert receive[0]['content'] == {
+            'contentType': 1,
+            'text': 'hello',
+            'toType': 1,
+        }
